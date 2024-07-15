edc5d03dd439867f7012a4622be919d59fda6b17
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-15 20:02:34 +0300

    url şemaları ana projede oluşturuldu

--- CourseApp/urls.py
+++ CourseApp/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+# http://127.0.0.1:8000/          -> Anasayfa
+# http://127.0.0.1:8000/anasayfa  -> Anasayfa
+# http://127.0.0.1:8000/kurslar   -> Kurslar Listesi
+
+def home(request):
+    return HttpResponse("Anasayfa")
+
+def courses(request):
+    return HttpResponse("Kurslar Listesi")
+
 urlpatterns = [
+    path("", home),
+    path("anasayfa", home),
+    path("kurslar", courses),
     path("admin/", admin.site.urls),
 ]
