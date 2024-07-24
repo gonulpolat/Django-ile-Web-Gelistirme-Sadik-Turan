1f7a749087a059a21e9fe6e86916b4bc3508c7d8
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-24 09:59:52 +0300

    Category modeli yönetim paneline eklendi

--- Courses/models.py
+++ Courses/models.py
@@ -22,3 +22,6 @@ class Course(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
