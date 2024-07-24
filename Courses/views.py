764fb49c18ae30009a89035daa5eac5b4aad9fcd
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-24 08:38:08 +0300

    kurs detay sayfaları id yerine slug ile oluşturuldu

--- Courses/views.py
+++ Courses/views.py
@@ -96,9 +96,9 @@ def index(request):
         "categories": kategoriler
     })
 
-def details(request, course_id):
+def details(request, slug):
     
-    course = get_object_or_404(Course, pk=course_id)
+    course = get_object_or_404(Course, slug=slug)
 
     context = {
         "course": course
