667aa8aee752a1d17522d0c09976079efa8a8657
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-24 08:11:51 +0300

    default save methodu tanımlandı

--- Courses/models.py
+++ Courses/models.py
@@ -8,9 +8,14 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=100)
     date = models.DateField()
     isActive = models.BooleanField()
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
         return f"{self.title}"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
     
 class Category(models.Model):
     name = models.CharField(max_length=50)
