8c28f9efcaf3f2296638f278d4d63370be77c869
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-27 11:16:59 +0300

    kurs ekleme sayfası değiştirildi

--- Courses/forms.py
+++ Courses/forms.py
@@ -4,7 +4,8 @@ class CourseCreateForm(forms.Form):
     title = forms.CharField(
         label="Başlık", 
         required=True,
-        error_messages={"required": "Kurs başlığı zorunludur."}
+        error_messages={"required": "Kurs başlığı zorunludur."},
+        widget=forms.TextInput()  # default widget
         )
     description = forms.CharField(widget=forms.Textarea)
     imageUrl = forms.CharField()
