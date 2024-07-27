83eff818990f2db7be9b7554338c5f1c029c55e7
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-27 11:01:39 +0300

    form class validation: title için property'ler eklendi

--- Courses/forms.py
+++ Courses/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(
+        label="Başlık", 
+        required=True,
+        error_messages={"required": "Kurs başlığı zorunludur."}
+        )
     description = forms.CharField(widget=forms.Textarea)
     imageUrl = forms.CharField()
     slug = forms.SlugField()
