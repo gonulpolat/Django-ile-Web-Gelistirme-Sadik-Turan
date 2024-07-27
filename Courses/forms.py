0ecfb2fb1187d00da1c36b86c2a311114bb87f37
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-27 16:42:03 +0300

    id bilgisine göre kurs güncelleme sayfası oluşturuldu

--- Courses/forms.py
+++ Courses/forms.py
@@ -33,3 +33,34 @@ class CourseCreateForm(forms.ModelForm):
                 'max_length': 'Fotoğraf URL\'si en fazla 50 karakter olabilir.'
             }
         }
+
+
+class CourseEditForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('title', 'description', 'imageUrl', 'slug')
+        labels = {
+            'title': 'Title',
+            'description': 'Description',
+            'imageUrl': 'Image URL',
+            'slug': 'Slug'
+        }
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+            'imageUrl': TextInput(attrs={'class': 'form-control'}),
+            'slug': TextInput(attrs={'class': 'form-control'})
+        }
+        error_messages = {
+            'title': {
+                'required': 'Kurs başlığı girmelisiniz.',
+                'max_length': 'Kurs başlığı en fazla 50 karakter olabilir.'
+            },
+            'description': {
+                'required': 'Kurs açıklaması girmelisiniz.'
+            },
+            'imageUrl': {
+                'required': 'Fotoğraf eklemelisiniz.',
+                'max_length': 'Fotoğraf URL\'si en fazla 50 karakter olabilir.'
+            }
+        }
--- Courses/views.py
+++ Courses/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
-from Courses.forms import CourseCreateForm
+from Courses.forms import CourseCreateForm, CourseEditForm
 from .models import Category, Course
 
 # Create your views here.
@@ -59,7 +59,13 @@ def courseList(request):
     })
 
 def courseEdit(request, id):
-    pass
+    course = get_object_or_404(Course, pk=id)
+
+    form = CourseEditForm(instance=course)
+
+    return render(request, "courses/course_edit.html", {
+        "form": form
+    })
 
 def details(request, slug):
     
