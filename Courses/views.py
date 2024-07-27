090bb50fe7853154d2d5005c281d85c71b8b41d1
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-27 10:18:59 +0300

    form class'ından post formu oluşturuldu

--- Courses/views.py
+++ Courses/views.py
@@ -33,7 +33,22 @@ def search(request):
     })
 
 def createCourse(request):
-    # GET sourgusu için çalışır
+    
+    if request.method == "POST":
+        form = CourseCreateForm(request.POST)
+
+        if form.is_valid():
+
+            course = Course(
+                title=form.cleaned_data["title"],
+                description=form.cleaned_data["description"],
+                imageUrl=form.cleaned_data["imageUrl"],
+                slug=form.cleaned_data["slug"]
+            )
+
+            course.save()
+
+            return redirect("kurs")
         
     form = CourseCreateForm()
 
