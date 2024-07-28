996f62fb75eb3d8e5edad145c41c769609346cb7
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-28 15:59:24 +0300

    user register sayfası oluşturuldu

--- Account/views.py
+++ Account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
 # Create your views here.
@@ -23,7 +24,28 @@ def UserLogin(request):
         return render(request, 'account/login.html')
 
 def UserRegister(request):
-    return render(request, 'account/register.html')
+
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repassword = request.POST['repassword']
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, 'account/register.html', {'error': 'Username is already taken'})
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, 'account/register.html', {'error': 'Email is already taken'})
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+                    user.save()
+                    return redirect('login')
+        else:
+            return render(request, 'account/register.html', {'error': 'Passwords do not match'})
+
+    else:
+        return render(request, 'account/register.html')
 
 def UserLogout(request):
     logout(request)
