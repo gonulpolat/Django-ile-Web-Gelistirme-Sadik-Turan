8030662ae56c26c2926940f098d89d5b9ba0f396
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-28 16:44:07 +0300

    kurs eklemek için giriş zorunluluğu ile ilgili güncellemeler yapıldı

--- Account/views.py
+++ Account/views.py
@@ -17,7 +17,11 @@ def UserLogin(request):
 
         if user is not None:
             login(request, user)
-            return redirect('index')
+            nextUrl = request.GET.get('next', None)
+            if nextUrl is None:
+                return redirect('index')
+            else:
+                return redirect(nextUrl)
         else:
             return render(request, 'account/login.html', {'error': 'Invalid username or password'})
     else:
