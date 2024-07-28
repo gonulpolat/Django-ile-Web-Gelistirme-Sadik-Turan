b5f7696f455a3e9fa03b31bf1e5950cfd68a14f8
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-28 13:22:33 +0300

    account için url şemaları ve sayfalar oluşturuldu

---
+++ Account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from Account import views
+
+
+urlpatterns = [
+    path('login', views.UserLogin, name='login'),
+    path('register', views.UserRegister, name='register'),
+    path('logout', views.UserLogout, name='logout')
+]
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def UserLogin(request):
+    return render(request, 'account/login.html')
+
+def UserRegister(request):
+    return render(request, 'account/register.html')
+
+def UserLogout(request):
+    return redirect("index")
