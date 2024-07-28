1a6bcdd0c9be188251f01ecac61b1cee4ae5796d
gonulpolat <190315065@ogr.cbu.edu.tr>
2024-07-28 13:57:04 +0300

    user login sayfası oluşturuldu

--- Account/views.py
+++ Account/views.py
@@ -1,9 +1,23 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 
 # Create your views here.
 
 def UserLogin(request):
-    return render(request, 'account/login.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'account/login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'account/login.html')
 
 def UserRegister(request):
     return render(request, 'account/register.html')
